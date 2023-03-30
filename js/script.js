4c7299b8f853b3aaef95f9d230f37341e616771a
nicolozibra1 <nico.zibra@gmail.com>
2023-03-30 22:43:08 +0200

    add-price

--- js/script.js
+++ js/script.js
@@ -14,71 +14,83 @@ createApp({
                 {
                     id:1,
                     name: 'bread',
+                    price: 1.99,
                     image: './img/bread.jpg',
                     completed: false,
                 },
                 {
                     id:2,
+                    price: 1.39,
                     name: 'milk',
                     image: './img/milk.jpg',
                     completed: false
                 },
                 {
                     id:3,
+                    price: 2.79,
                     name: 'egg',
                     image: './img/egg.jpg',
                     completed: false
                 },
                 {
                     id:4,
+                    price: 2.99,
                     name: 'hamburger',
                     image: './img/hamburger.jpg',
                     completed: false
                 },
                 {
                     id:5,
+                    price: 1.99,
                     name: 'apple',
                     image: './img/apple.jpg',
                     completed: false
                 },
                 {
                     id:6,
+                    price: 0.99,
                     name: 'lettuce iceberg',
                     image: './img/lattuce.jpg',
                     completed: false
                 },
                 {
                     id:7,
+                    price: 3.29,
                     name: 'rice',
                     image: './img/rice.jpg',
                     completed: false
                 },
                 {
                     id:8,
+                    price: 1.49,
                     name: 'pasta',
                     image: './img/pasta.jpg',
                     completed: false
                 },
                 {
                     id:9,
+                    price: 5.49,
                     name: 'cheese',
                     image: './img/cheese.jpg',
                     completed: false
                 },
                 {
                     id:10,
+                    price: 1.29,
                     name: 'yogurt',
                     image: './img/yogurt.jpg',
                     completed: false
                 },
                 {
                     id:11,
+                    price: 1.95,
                     name: 'seed oil',
                     image: './img/seed-oil.jpg',
                     completed: false
                 },
                 {
                     id:12,
+                    price: 1.33,
                     name: 'mais',
                     image: './img/mais.jpg',
                     completed: false
